bf7a0ea77fb4bfd7e91e63fa3503f3afcaf1ddd3
Quang-1310 <hoangminhquang1310@gmail.com>
2026-02-24 07:45:12 +0700

    Add files via upload

---
+++ PTIT_CNTT2_IT203_Session11_Exam/src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Drink[] drinks = new Drink[3];
+        drinks[0] = new Coffee("C001", "Bạc sỉu", 30000, true);
+        drinks[1] = new FruitJuice("C002", "Nước cam", 40000, 10);
+        drinks[2] = null;
+        for(int i = 0; i < 3; i++){
+            if(drinks[i] == null){
+                continue;
+            }
+            drinks[i].displayInfor();
+            System.out.println("Thành tiền " + drinks[i].calculatePrice());
+            if(drinks[i] instanceof FruitJuice){
+                ((FruitJuice) drinks[i]).mix();
+            }
+        }
+    }
+}
